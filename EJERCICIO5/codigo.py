3579e60bc2446e30fbca4cb23f62ad725ecb86ff
xmachos <sega3972@gmail.com>
2026-03-15 17:04:18 -0400

    Ejercicio 4 y 5

--- EJERCICIO5/codigo.py
+++ EJERCICIO5/codigo.py
@@ -1,24 +1,29 @@
-class Bus:
+#No sabia de que enviar
+class Aula:
+    def __init__(self, nombreAula, piso):
+        self.nombreAula = nombreAula
+        self.piso = piso
+        self.estudiantes = []
 
-    def __init__(self, capacidad):
-        self.capacidad = capacidad
-        self.pasajeros = 0
-        self.dinero = 0
-    def subir_pasajeros(self, x):
-        if self.pasajeros + x <= self.capacidad:
-            self.pasajeros = self.pasajeros + x
-            print("Subieron", x, "pasajeros")
-        else:
-            print("No hay suficientes asientos")
-    def cobrar_pasaje(self):
-        costo = 1.50
-        total = self.pasajeros * costo
-        self.dinero = self.dinero + total
-        print("Dinero cobado:", total)
-    def asientos_disponibles(self):
-        disponibles = self.capacidad - self.pasajeros
-        print("Asientos dsponibles:", disponibles)
-bus1 = Bus(40)
-bus1.subir_pasajeros(10)
-bus1.cobrar_pasaje()
-bus1.asientos_disponibles()
+    def agregarEstudiante(self, nombre, nota):
+        self.estudiantes.append([nombre, nota])
+
+    def mostrarDatos(self):
+        print(self.nombreAula, self.piso)
+        for e in self.estudiantes:
+            print(e[0], e[1])
+
+    def mostrarEstado(self):
+        for e in self.estudiantes:
+            if e[1] >= 51:
+                print(e[0], e[1], "APEROBADO")
+            else:
+                print(e[0], e[1], "REPROBADO")
+
+
+
+a = Aula("Aula 101", 1)
+a.agregarEstudiante("Luis", 67)
+a.agregarEstudiante("Aracely", 89)
+a.mostrarDatos()
+a.mostrarEstado()
